221af65e6ab409270c48b95c6caa954979a34c03
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-15 23:50:09 +0530

    Total and Amount functionality Added.

--- src/reducer.js
+++ src/reducer.js
@@ -25,20 +25,56 @@ const reducer = (state, action) => {
   }
 
   if (action.type === 'DECREASE') {
-    const tempCart = state.cart.map((item) => {
-      if (item.id === action.payload) {
-        return {
-          ...item,
-          amount: item.amount - 1,
-        };
-      }
-      return item;
-    });
+    const tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            amount: item.amount - 1,
+          };
+        }
+        return item;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
     return {
       ...state,
       cart: tempCart,
     };
   }
+  if (action.type === 'GET_TOTALS') {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+        cartTotal.total += itemTotal;
+        cartTotal.amount += amount;
+
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+
+  if (action.type === 'LOADING') {
+    return {
+      ...state,
+      loading: true,
+    };
+  }
+  if (action.type === 'DISPLAY_ITEMS') {
+    return {
+      ...state,
+      cart: action.payload,
+      loading: false,
+    };
+  }
+
   return state;
 };
 
