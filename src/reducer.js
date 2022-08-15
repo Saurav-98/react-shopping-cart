843075e707739eca6fc3d069d90a7f57f52c8fbb
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-15 22:53:29 +0530

    Clear Button and Remove Button functionality added

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,16 @@
-const reducre = (state, action) => {
+const reducer = (state, action) => {
+  if (action.type === 'CLEAR_CART') {
+    return {
+      ...state,
+      cart: [],
+    };
+  }
+  if (action.type === 'REMOVE') {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
   return state;
 };
 
